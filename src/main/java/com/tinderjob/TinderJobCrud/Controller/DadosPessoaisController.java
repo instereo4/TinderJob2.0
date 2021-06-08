68b44fa6966911f4a94fad99dc43b5ae46a308b5
Edupontes <edupontes.devsforce@gmail.com>
2021-06-08 08:55:26 -0300

    Create DadosPessoaisController.java

---
+++ src/main/java/com/tinderjob/TinderJobCrud/Controller/DadosPessoaisController.java
@@ -0,0 +1,29 @@
+package com.tinderjob.TinderJobCrud.Controller;
+
+import java.util.List;
+
+import com.tinderjob.TinderJobCrud.InterfaceService.DadosPessoaisInterfServ;
+import com.tinderjob.TinderJobCrud.Model.DadosPessoais;
+import com.tinderjob.TinderJobCrud.Repository.DadosPessoaisRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class DadosPessoaisController {
+    
+    @Autowired
+    private DadosPessoaisInterfServ action;
+
+    @GetMapping("/listar")
+    public String listar(Model model){
+        List<DadosPessoais> dados = action.listar();
+        model.addAttribute("dados_pessoais", dados);
+        return "index";
+    }
+
+}
